0181ff0481201ace471866eb3cc0726612a23093
Gabriel Barbosa <gabriel@howtomaximize.com.br>
2014-04-12 20:00:14 -0300

    Migrating to grunt

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-## ng-highcharts
+## ng-highcharts [![Build Status](https://travis-ci.org/bielversallini/ng-highcharts.svg)](https://travis-ci.org/bielversallini/ng-highcharts)
 Angular directive for Highcharts
 
 > **WORK IN PROGRESS!** 
+
+How to use
+---------
+
+**1) Adding the library:**
+	
+	<script src="ngHighcharts.min.js"></script>
+	
+**2) Injecting in your app:**
+	
+	angular.module('myApp',['ngHighcharts'])
+
+
+**3) Now! Use the tag < highchart > anywhere:**
+	
+	<highchart type="column" data="arr"
+               title="Movie Audience Measurement"
+               subtitle="Source: PirateBay"
+               y-title="Quantitative (MM)"
+               categories="['Jan', 'Feb', 'Mar', 'Apr', 'May']">
+    </highchart>
+    
+Only types column, bar, line, area and scatter are available.
+
+**4) See!**
+
+![image](https://raw.github.com/bielversallini/ng-highcharts/master/sample.png)
+
 
 ## License
 
---
+++ dist/ng-highcharts.js
@@ -0,0 +1,114 @@
+/**
+ * ng-highcharts
+ * @version v0.1.0 - 2014-04-12
+ * @link https://github.com/bielversallini/ng-highcharts
+ * @author Gabriel Barbosa <gabriel@versallini.com.br>
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+(function() {
+    'use strict';
+    angular.module('ngHighcharts', []);
+    angular.module('ngHighcharts').directive('highchart', function($log, highchart) {
+        return {
+            restrict: 'ACE',
+            replace: true,
+            template: '<div></div>',
+            scope: {
+                data: '=',
+                options: '='
+            },
+            link: function(scope, element, attrs) {
+
+                var type = 'line';
+                var title = '';
+                var subtitle = '';
+                var xAxisTitle = '';
+                var yAxisTitle = '';
+                var xAxisCategories = null;
+                var height = 600;
+
+                if (attrs.type) {
+                    type = attrs.type.toLowerCase();
+                }
+                if (attrs.title) {
+                    title = attrs.title;
+                }
+                if (attrs.subtitle) {
+                    subtitle = attrs.subtitle;
+                }
+                if (attrs.xTitle) {
+                    xAxisTitle = attrs.xTitle;
+                }
+                if (attrs.yTitle) {
+                    yAxisTitle = attrs.yTitle;
+                }
+                if (attrs.categories) {
+                    xAxisCategories = attrs.categories;
+                }
+                if (attrs.height) {
+                    height = eval(attrs.height);
+                }
+
+                var chartOptions = highchart.defaultOptions();
+                if (scope.options) {
+                    chartOptions = scope.options;
+                }
+                if (chartOptions) {
+                    chartOptions.chart.type = type;
+                    chartOptions.title.text = title;
+                    chartOptions.subtitle.text = subtitle;
+                    chartOptions.xAxis.title.text = xAxisTitle;
+                    chartOptions.yAxis.title.text = yAxisTitle;
+                    chartOptions.xAxis.categories = eval(xAxisCategories);
+                    chartOptions.chart.height = height;
+                }
+
+                scope.$watch('data', function(val) {
+                    if (chartOptions && chartOptions.series) {
+                        chartOptions.series = val;
+                        element.highcharts(chartOptions);
+                    }
+                });
+
+            }
+        }
+    });
+
+    angular.module('ngHighcharts').factory('highchart', function() {
+        return {
+            defaultOptions: function() {
+                return {
+                    chart: {
+                        animation: true,
+                        type: 'line'
+                    },
+                    title: {
+                        text: ''
+                    },
+                    subtitle: {
+                        text: ''
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    legend: {
+                        labelFormat: '{name}'
+                    },
+                    series: [],
+                    xAxis: {
+                        categories: [],
+                        title: {
+                            text: 'sdsadsadsad'
+                        },
+                    },
+                    yAxis: {
+                        categories: null,
+                        title: {
+                            text: ''
+                        }
+                    }
+                }
+            }
+        }
+    });
+})();
